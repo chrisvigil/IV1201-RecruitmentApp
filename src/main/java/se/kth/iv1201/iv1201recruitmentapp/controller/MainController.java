8e1adfec141857d05fe43abf8f12cd2c5843d9f6
David Strömqvist <55108496+David-Stromqvist@users.noreply.github.com>
2022-02-24 09:19:32 +0100

    Added controller for test of exception handling

--- src/main/java/se/kth/iv1201/iv1201recruitmentapp/controller/MainController.java
+++ src/main/java/se/kth/iv1201/iv1201recruitmentapp/controller/MainController.java
@@ -1,10 +1,13 @@
 package se.kth.iv1201.iv1201recruitmentapp.controller;
 
+import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.SQLException;
 
 /**
  * The controller for simple Get requests.
@@ -60,7 +63,21 @@ public class MainController {
     }
 
 
-    //TODO
+    /**
+     * Mapping for database connection error page needed for Authentication error handling
+     *
+     * @return The error page.
+     */
     @GetMapping("/error/dbConnectionError")
-    public String dbError(){ return "/error/dbConnectionError";}
+    public String dbError () { return "/error/dbConnectionError";}
+
+    /**
+     * Test controller to show that if error is thrown the right error handler will take care of it.
+     * @return nothing, an exception is thrown.
+     */
+    @GetMapping("/error/test")
+    public String er () {
+        SQLException test = new SQLException("sql test");
+        throw new JDBCConnectionException("test", test);
+    }
 }
