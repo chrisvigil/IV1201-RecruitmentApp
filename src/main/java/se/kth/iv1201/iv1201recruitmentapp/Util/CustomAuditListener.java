5ce82b7424ad9b2413d387bf88a790503fb7037d
David Strömqvist <55108496+David-Stromqvist@users.noreply.github.com>
2022-03-07 19:57:49 +0100

    added comments

--- src/main/java/se/kth/iv1201/iv1201recruitmentapp/Util/CustomAuditListener.java
+++ src/main/java/se/kth/iv1201/iv1201recruitmentapp/Util/CustomAuditListener.java
@@ -7,11 +7,17 @@ import org.springframework.boot.actuate.audit.listener.AuditApplicationEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-
+/**
+ * Listeners for logging events
+ */
 @Component
 public class CustomAuditListener {
     private Logger logger = LoggerFactory.getLogger("security");
 
+    /**
+     * Logging of authorization and authentication events from spring security.
+     * @param event event to listen to
+     */
     @EventListener
     public void onAuditEvent(AuditApplicationEvent event) {
         AuditEvent auditEvent = event.getAuditEvent();
--- src/main/java/se/kth/iv1201/iv1201recruitmentapp/config/AuditEventConfiguration.java
+++ src/main/java/se/kth/iv1201/iv1201recruitmentapp/config/AuditEventConfiguration.java
@@ -5,14 +5,27 @@ import org.springframework.boot.actuate.security.AuthorizationAuditListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Beans so that Listeners can be implemented for AuditEvents.
+ */
 @Configuration
 public class AuditEventConfiguration {
 
+    /**
+     * Used for making authentication listeners possible.
+     * @return an authentication listener
+     * @throws Exception if no listener could be created.
+     */
     @Bean
     public AuthenticationAuditListener authenticationAuditListener() throws Exception {
         return new AuthenticationAuditListener();
     }
 
+    /**
+     * Used for making authorization listeners possible.
+     * @return an authorization listener
+     * @throws Exception if no listener could be created
+     */
     @Bean
     public AuthorizationAuditListener authorizationAuditListener() throws  Exception {
         return new AuthorizationAuditListener();
