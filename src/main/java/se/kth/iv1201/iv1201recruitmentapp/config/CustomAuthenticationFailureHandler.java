169984a3aced12197d8999a11e3282518afa8a87
David Strömqvist <55108496+David-Stromqvist@users.noreply.github.com>
2022-03-08 10:01:38 +0100

    Logging of errors caught by errorhandling controller

--- src/main/java/se/kth/iv1201/iv1201recruitmentapp/config/CustomAuthenticationFailureHandler.java
+++ src/main/java/se/kth/iv1201/iv1201recruitmentapp/config/CustomAuthenticationFailureHandler.java
@@ -1,5 +1,7 @@
 package se.kth.iv1201.iv1201recruitmentapp.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.DefaultRedirectStrategy;
@@ -17,6 +19,7 @@ import java.io.IOException;
  */
 @Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    Logger logger = LoggerFactory.getLogger(CustomAuthenticationFailureHandler.class);
 
     /**
      * Called when an authentication attempt fails. Redirects to a special error page if connection to the database fails.
@@ -34,6 +37,8 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
         String url;
         if(exception instanceof InternalAuthenticationServiceException) {
             url = "/error/dbConnectionError";
+            logger.error("Communication error with the database: " + exception.toString());
+            exception.printStackTrace();
         }
         else {
             url = "/login?error";
